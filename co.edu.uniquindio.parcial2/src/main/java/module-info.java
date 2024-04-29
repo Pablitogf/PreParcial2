0f7834f70da4757755d93cb261be1c953ff376dd
Pablo Gonzalez Florez <142361522+Pablitogf@users.noreply.github.com>
2024-04-28 20:53:31 -0500

    Inicio

---
+++ co.edu.uniquindio.parcial2/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module co.edu.uniquindio.parcial2.parcial2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens co.edu.uniquindio.parcial2.parcial2 to javafx.fxml;
+    exports co.edu.uniquindio.parcial2.parcial2;
+}
