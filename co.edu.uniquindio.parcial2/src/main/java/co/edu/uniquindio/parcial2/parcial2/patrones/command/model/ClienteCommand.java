bade2de1ef4aa51548dacffc32a467e54569b41a
Pablo Gonzalez Florez <142361522+Pablitogf@users.noreply.github.com>
2024-04-30 12:22:20 -0500

    ac

--- co.edu.uniquindio.parcial2/src/main/java/co/edu/uniquindio/parcial2/parcial2/patrones/command/model/ClienteCommand.java
+++ co.edu.uniquindio.parcial2/src/main/java/co/edu/uniquindio/parcial2/parcial2/patrones/command/model/ClienteCommand.java
@@ -2,20 +2,17 @@ package co.edu.uniquindio.parcial2.parcial2.patrones.command.model;
 
 import co.edu.uniquindio.parcial2.parcial2.patrones.command.services.Command;
 
-public class Cliente {
+public class ClienteCommand {
+
     public static void main(String[] args) {
-        // Crear receptor
         Receptor receptor = new Receptor();
 
-        // Crear comandos de entrega
         Command entregaLocal = new EntregaLocalCommand(receptor);
         Command entregaNacional = new EntregaNacionalCommand(receptor);
         Command entregaInternacional = new EntregaInternacionalCommand(receptor);
 
-        // Crear invocador
         Invocador invocador = new Invocador();
 
-        // Ejecutar comandos de entrega
         invocador.ejecutarComando(entregaLocal);
         invocador.ejecutarComando(entregaNacional);
         invocador.ejecutarComando(entregaInternacional);
